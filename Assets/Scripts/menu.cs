1316118bd7310ab7ad5371b069f9360364f2929e
TheR2D2cx <63327126+TheR2D2cx@users.noreply.github.com>
2021-10-24 18:02:56 -0700

    Proyecto init
    Menu y escenario principal (no terminado)

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
